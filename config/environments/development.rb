c6b9fac20541c67eb4749c5ebfaeae97d8154bb4
Ryan MacGillivray <rmacg89@gmail.com>
2015-02-15 16:21:25 +0000

    use mailcatcher in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Use mailcatcher for emails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
